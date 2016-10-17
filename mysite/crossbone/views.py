bdb6c5a7163b25de32a96c1c98a72b72237e0aa0
Douglas Morais <msantos.douglas@gmail.com>
2016-10-17 21:17:14 -0200

    Added Events and Groups inserts MD

--- mysite/crossbone/views.py
+++ mysite/crossbone/views.py
@@ -63,7 +63,7 @@ def group(request):
     return HttpResponse(template.render(content, request))
 
 
-def group_new(request):
+def group_new(request): #testado MD
 
 	template = loader.get_template('home/group_new.html')
 
@@ -96,7 +96,7 @@ def group_new(request):
 		for input_name in users_group_input_names:
 			group_users.append(request.POST.get(input_name))
 			group_users_hashs = input_name.replace('user-group','')
-			group_roles.append(request.POST.getlist('user-role'+group_users_hashs+'-multiple'))
+			group_roles.append(request.POST.getlist('group-role'+group_users_hashs+'-multiple'))
 
 		if group_origin:
 			document_group_origin = Groups.objects.get(id=ObjectId(group_origin))
@@ -127,22 +127,18 @@ def group_new(request):
 			for role in document_group_users_roles[key]:
 				user_roles.append(User_roles(user=user, role=role))
 
-		group = Groups.objects.create(
-			group_type=[group_type],
-			origin=document_group_origin,
-			groups_over=document_group_acima,
-			groups_under=document_group_abaixo,
-			user_roles = user_roles,			
-			data={'name':group_name}
-		)
 
-		return HttpResponse('abc')
+		group = Groups()
+		group.add_group({'name':group_name}, [group_type], document_group_origin, document_group_acima, document_group_abaixo, user_roles)
+
+
+		return HttpResponse('ok')
 
 	else:
 
 		return HttpResponse(template.render(content, request))
 
-def user_new(request):
+def user_new(request): #testado MD
 
 	template = loader.get_template('home/user_new.html')
 
@@ -168,7 +164,7 @@ def user_new(request):
 
 
 
-def role_new(request):
+def role_new(request): #testado MD
 
 	template = loader.get_template('home/role_new.html')
 
@@ -196,7 +192,7 @@ def role_new(request):
 		return HttpResponse(template.render(content, request))
 
 
-def grouptype_new(request):
+def grouptype_new(request): #testado MD
 
 	template = loader.get_template('home/grouptype_new.html')
 
@@ -222,20 +218,64 @@ def grouptype_new(request):
 		return HttpResponse(template.render(content, request))
 
 
-def event_new(request):
+def event_new(request): 
 
-    template = loader.get_template('home/event_new.html')
+	template = loader.get_template('home/event_new.html')
+
+	content = {
+		'Users': Users.objects,
+		'Groups': Groups.objects,
+		'Groups_types': Groups_types.objects,
+		'Roles': Roles.objects,
+		'Events': Events.objects,        
+	}
+
+	if request.method == 'POST':
+
+		document_group_origin, document_group_acima, document_group_abaixo = None, [], []
+		document_group_roles, document_group_users, document_group_users_roles = [], [], []
+		group_users = []
+		group_users_hashs = []
+		group_roles = []
+		user_roles = []
+		event_name = request.POST.get('nome-evento')
+		group_origin = request.POST.get('grupo-origem-evento')
+
+		
+		users_group_input_names = [name for name in request.POST.keys() if name.startswith('user-group')]
+		for input_name in users_group_input_names:
+			group_users.append(request.POST.get(input_name))
+			group_users_hashs = input_name.replace('user-group','')
+			group_roles.append(request.POST.getlist('group-role'+group_users_hashs+'-multiple'))
+
+		if group_origin:
+			document_group_origin = Groups.objects.get(id=ObjectId(group_origin))
+
+		if group_users:
+			for key, users in enumerate(group_users):
+				if users != '':
+					document_group_users.append(Users.objects.get(id=ObjectId(group_users[key])))					
 
 
-    content = {
-        'Users': Users.objects,
-        'Groups': Groups.objects,
-        'Groups_types': Groups_types.objects,
-        'Roles': Roles.objects,
-        'Events': Events.objects,        
-    }
+					for roles in group_roles[key]:
+						document_group_roles.append(Roles.objects.get(id=ObjectId(roles)))
 
-    return HttpResponse(template.render(content, request))        
+					document_group_users_roles.append(document_group_roles)
+					document_group_roles = []		
+
+		for key, user in enumerate(document_group_users):
+
+			for role in document_group_users_roles[key]:
+				user_roles.append(User_roles(user=user, role=role))
+
+		evento = Events()
+		evento.add_event(document_group_origin, user_roles, {'name':event_name})
+
+		return HttpResponse('evento ok')    
+
+	else:
+
+		return HttpResponse(template.render(content, request))        
 
 def event(request):
 
