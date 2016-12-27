3f8eb37d4b577378e0db3fb7a5513ac1db6e6bbb
Douglas Morais <msantos.douglas@gmail.com>
2016-12-27 16:21:07 -0200

    Added Group/Ungroup switch on cell graph

--- apps/cell/templates/js/cell_detail.js
+++ apps/cell/templates/js/cell_detail.js
@@ -58,7 +58,7 @@ function initMap() {
 */
 //var randomScalingFactor = function(){ return Math.round(Math.random()*10)};
 var trendingLineChart;
-var data = {
+window.dataLine1 = {
     labels : {{ presence_days|safe }},
     datasets : [
         {
@@ -93,6 +93,71 @@ var data = {
         }        
     ]
 };
+
+window.dataLine2 = {
+    labels : {{ presence_days|safe }},
+    datasets : [
+        {
+            label: "Presentes",
+            fillColor : "rgba(128, 222, 234, 0.6)",
+            strokeColor : "#ffffff",
+            pointColor : "#FFbcd4",
+            pointStrokeColor : "#ffffff",
+            pointHighlightFill : "#ffffff",
+            pointHighlightStroke : "#ffffff",
+            data: {{ roles_presence.member|safe }}
+        },
+        {
+            label: "Ausentes",
+            fillColor : "rgba(128, 222, 234, 0.6)",
+            strokeColor : "#ffffff",
+            pointColor : "#FFbcd4",
+            pointStrokeColor : "#ffffff",
+            pointHighlightFill : "#ffffff",
+            pointHighlightStroke : "#ffffff",
+            data: {{ roles_presence.leader|safe }}
+        },            
+    ]
+};
+
+window.optionsLine = {  
+        multiTooltipTemplate: "<%= datasetLabel %>:  <%= value %>",
+        scaleShowGridLines : true,///Boolean - Whether grid lines are shown across the chart        
+        scaleGridLineColor : "rgba(255,255,255,0.4)",//String - Colour of the grid lines        
+        scaleGridLineWidth : 1,//Number - Width of the grid lines       
+        scaleShowHorizontalLines: true,//Boolean - Whether to show horizontal lines (except X axis)     
+        scaleShowVerticalLines: false,//Boolean - Whether to show vertical lines (except Y axis)        
+        bezierCurve : true,//Boolean - Whether the line is curved between points        
+        bezierCurveTension : 0.4,//Number - Tension of the bezier curve between points      
+        pointDot : true,//Boolean - Whether to show a dot for each point        
+        pointDotRadius : 5,//Number - Radius of each point dot in pixels        
+        pointDotStrokeWidth : 2,//Number - Pixel width of point dot stroke      
+        pointHitDetectionRadius : 20,//Number - amount extra to add to the radius to cater for hit detection outside the drawn point        
+        datasetStroke : true,//Boolean - Whether to show a stroke for datasets      
+        datasetStrokeWidth : 3,//Number - Pixel width of dataset stroke     
+        datasetFill : true,//Boolean - Whether to fill the dataset with a colour                
+        animationSteps: 100,// Number - Number of animation steps        
+        animationEasing: "easeOutQuart",// String - Animation easing effect         
+        tooltipTitleFontFamily: "'Roboto','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",// String - Tooltip title font declaration for the scale label        
+        scaleFontSize: 12,// Number - Scale label font size in pixels       
+        scaleFontStyle: "normal",// String - Scale label font weight style      
+        scaleFontColor: "#fff",// String - Scale label font colour
+        tooltipEvents: ["mousemove", "touchstart", "touchmove"],// Array - Array of string names to attach tooltip events       
+        tooltipFillColor: "rgba(255,255,255,0.8)",// String - Tooltip background colour     
+        tooltipTitleFontFamily: "'Roboto','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",// String - Tooltip title font declaration for the scale label        
+        tooltipFontSize: 12,// Number - Tooltip label font size in pixels
+        tooltipFontColor: "#000",// String - Tooltip label font colour      
+        tooltipTitleFontFamily: "'Roboto','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",// String - Tooltip title font declaration for the scale label        
+        tooltipTitleFontSize: 14,// Number - Tooltip title font size in pixels      
+        tooltipTitleFontStyle: "bold",// String - Tooltip title font weight style       
+        tooltipTitleFontColor: "#000",// String - Tooltip title font colour     
+        tooltipYPadding: 8,// Number - pixel width of padding around tooltip text       
+        tooltipXPadding: 16,// Number - pixel widtho f padding around tooltip text      
+        tooltipCaretSize: 10,// Number - Size of the caret on the tooltip       
+        tooltipCornerRadius: 6,// Number - Pixel radius of the tooltip border       
+        tooltipXOffset: 10,// Number - Pixel offset from point x to tooltip edge
+        responsive: true,
+        };
 
 /*
 Polor Chart Widget
@@ -143,44 +208,7 @@ var dataBarChart = {
 
 window.onload = function(){
     var trendingLineChart = document.getElementById("trending-line-chart").getContext("2d");
-    window.trendingLineChart = new Chart(trendingLineChart).Line(data, {  
-        multiTooltipTemplate: "<%= datasetLabel %>:  <%= value %>",
-        scaleShowGridLines : true,///Boolean - Whether grid lines are shown across the chart        
-        scaleGridLineColor : "rgba(255,255,255,0.4)",//String - Colour of the grid lines        
-        scaleGridLineWidth : 1,//Number - Width of the grid lines       
-        scaleShowHorizontalLines: true,//Boolean - Whether to show horizontal lines (except X axis)     
-        scaleShowVerticalLines: false,//Boolean - Whether to show vertical lines (except Y axis)        
-        bezierCurve : true,//Boolean - Whether the line is curved between points        
-        bezierCurveTension : 0.4,//Number - Tension of the bezier curve between points      
-        pointDot : true,//Boolean - Whether to show a dot for each point        
-        pointDotRadius : 5,//Number - Radius of each point dot in pixels        
-        pointDotStrokeWidth : 2,//Number - Pixel width of point dot stroke      
-        pointHitDetectionRadius : 20,//Number - amount extra to add to the radius to cater for hit detection outside the drawn point        
-        datasetStroke : true,//Boolean - Whether to show a stroke for datasets      
-        datasetStrokeWidth : 3,//Number - Pixel width of dataset stroke     
-        datasetFill : true,//Boolean - Whether to fill the dataset with a colour                
-        animationSteps: 15,// Number - Number of animation steps        
-        animationEasing: "easeOutQuart",// String - Animation easing effect         
-        tooltipTitleFontFamily: "'Roboto','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",// String - Tooltip title font declaration for the scale label        
-        scaleFontSize: 12,// Number - Scale label font size in pixels       
-        scaleFontStyle: "normal",// String - Scale label font weight style      
-        scaleFontColor: "#fff",// String - Scale label font colour
-        tooltipEvents: ["mousemove", "touchstart", "touchmove"],// Array - Array of string names to attach tooltip events       
-        tooltipFillColor: "rgba(255,255,255,0.8)",// String - Tooltip background colour     
-        tooltipTitleFontFamily: "'Roboto','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",// String - Tooltip title font declaration for the scale label        
-        tooltipFontSize: 12,// Number - Tooltip label font size in pixels
-        tooltipFontColor: "#000",// String - Tooltip label font colour      
-        tooltipTitleFontFamily: "'Roboto','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",// String - Tooltip title font declaration for the scale label        
-        tooltipTitleFontSize: 14,// Number - Tooltip title font size in pixels      
-        tooltipTitleFontStyle: "bold",// String - Tooltip title font weight style       
-        tooltipTitleFontColor: "#000",// String - Tooltip title font colour     
-        tooltipYPadding: 8,// Number - pixel width of padding around tooltip text       
-        tooltipXPadding: 16,// Number - pixel width of padding around tooltip text      
-        tooltipCaretSize: 10,// Number - Size of the caret on the tooltip       
-        tooltipCornerRadius: 6,// Number - Pixel radius of the tooltip border       
-        tooltipXOffset: 10,// Number - Pixel offset from point x to tooltip edge
-        responsive: true,
-        });
+    window.trendingLineChart = new Chart(trendingLineChart).Line(window.dataLine1, window.optionsLine);
 
         var doughnutChart = document.getElementById("doughnut-chart").getContext("2d");
         window.myDoughnut = new Chart(doughnutChart).Doughnut(doughnutData, {
@@ -204,7 +232,19 @@ window.onload = function(){
         });
 
 
-  
-
-
 };
+
+function updateGroupChart(){
+
+    if ($('#group-chart').prop('checked')){
+        window.trendingLineChart.destroy();
+        var trendingLineChart = document.getElementById("trending-line-chart").getContext("2d");        
+        window.trendingLineChart = new Chart(trendingLineChart).Line(window.dataLine2, window.optionsLine);
+        window.trendingLineChart.update();
+    }else{
+        window.trendingLineChart.destroy();        
+        var trendingLineChart = document.getElementById("trending-line-chart").getContext("2d");        
+        window.trendingLineChart = new Chart(trendingLineChart).Line(window.dataLine1, window.optionsLine);
+        window.trendingLineChart.update();
+    }
+}  
