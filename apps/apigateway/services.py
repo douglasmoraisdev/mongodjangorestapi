b64fa394203b3fb6bee98d4c07cde2e3d04b466e
Douglas Morais <msantos.douglas@gmail.com>
2017-02-10 12:37:10 +0000

    Updated apigateway/services.py

--- apps/apigateway/services.py
+++ apps/apigateway/services.py
@@ -1,10 +1,12 @@
 import requests 
 
+#TODO: Make requests internals works (localhost), is returning 400
 def get_books(year, author, req):
-    url = 'http://dmsantos-bethel-api-4356050/reuniaocelula/5899c6a7f6511923031c749a'
-    #url = 'https://bethel-api-dmsantos.c9users.io/reuniaocelula/5899c6a7f6511923031c749a' 
+    #url = 'http://localhost:8080/reuniaocelula/5899c6a7f6511923031c749a.json'
+    url = 'https://bethel-api-dmsantos.c9users.io/reuniaocelula/5899c6a7f6511923031c749a' 
     params = {'year': year, 'author': author}
     r = requests.get(url, params=params, headers={'Authorization': 'Bearer '+str(req.auth)})
+    print(r)
     books = r.json()
     books_list = {'books':books}
     return books_list
